8a019ab1088ee74f3ebaf2bed27db892e0ff8804
Majzerek <majzermariusz@gmail.com>
2024-12-11 13:59:07 +0100

    links

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Main />} path='/' />
+        <Route element={<Main />} path='https://majzerek.github.io/multi-app/' />
         <Route element={
           <TodoProvider>
             <ToDoList />
-          </TodoProvider>} path='/todo' />
+          </TodoProvider>} path='https://majzerek.github.io/multi-app//todo' />
       </Routes>
     </BrowserRouter>
   )
